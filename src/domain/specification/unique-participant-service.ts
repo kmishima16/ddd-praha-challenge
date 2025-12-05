c92aa3f003959626f11edca0dac099cbc988067c
三島 <ilfr1616@gmail.com>
2025-12-05 23:23:49 +0900

    participantからstudentに名称を統一

--- src/domain/specification/unique-participant-service.ts
+++
@@ -1,5 +0,0 @@
-import type { MailAddress } from "../model/student/value-object/mail-address";
-
-export interface IUniqueParticipantService {
-  isSatisfiedBy(email: MailAddress): Promise<boolean>;
-}
