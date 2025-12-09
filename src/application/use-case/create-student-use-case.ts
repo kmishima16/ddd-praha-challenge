339256a66134dc081504078b7f236ad177beae5d
三島 <ilfr1616@gmail.com>
2025-12-09 17:11:37 +0900

    ドメインサービスではなくアプリケーション層で実装するように変更

--- src/application/use-case/create-student-use-case.ts
+++ src/application/use-case/create-student-use-case.ts
@@ -2,7 +2,9 @@ import { Student } from "../../domain/model/student/student";
 import { MailAddress } from "../../domain/model/student/value-object/mail-address";
 import type { IStudentRepository } from "../../domain/repository/student-repository";
 import type { IUniqueStudentService } from "../../domain/specification/unique-student-service";
-import type { AssignAllLessonsToNewStudentService } from "../../domain/service/assign-all-lessons-to-new-student.service";
+import type { ILessonRepository } from "../../domain/repository/lesson-repository";
+import type { ILessonProgressRepository } from "../../domain/repository/lesson-progress-repository";
+import { LessonProgress } from "../../domain/model/lesson-progress/lesson-progress";
 
 export type CreateStudentUseCaseInput = {
   name: string;
@@ -19,14 +21,14 @@ export class CreateStudentUseCase {
   public constructor(
     private readonly studentRepository: IStudentRepository,
     private readonly uniqueStudentService: IUniqueStudentService,
-    private readonly assignAllLessonsToNewStudentService: AssignAllLessonsToNewStudentService,
+    private readonly lessonRepository: ILessonRepository,
+    private readonly lessonProgressRepository: ILessonProgressRepository,
   ) {}
 
   public async invoke(
     input: CreateStudentUseCaseInput,
   ): Promise<CreateStudentUseCasePayload> {
     const mailAddress = MailAddress.create(input.mailAddress);
-
     const isUnique = await this.uniqueStudentService.isSatisfiedBy(mailAddress);
     if (!isUnique) {
       throw new Error("mail address already exists");
@@ -34,7 +36,12 @@ export class CreateStudentUseCase {
 
     const student = Student.create(input.name, mailAddress);
     await this.studentRepository.save(student);
-    await this.assignAllLessonsToNewStudentService.execute(student.id);
+
+    const lessons = await this.lessonRepository.findAll();
+    const lessonProgresses = lessons.map((lesson) => {
+      return LessonProgress.create(student.id, lesson.id);
+    });
+    await this.lessonProgressRepository.saveAll(lessonProgresses);
 
     return {
       id: student.id.value,
