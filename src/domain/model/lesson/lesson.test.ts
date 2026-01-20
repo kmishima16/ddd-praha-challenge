1eb3444fa403cf10b7ec36433f80308b5da58ba9
kmishima <ilfr1616@gmail.com>
2026-01-21 02:11:23 +0900

    lessonCategoryId追加時のテスト

--- src/domain/model/lesson/lesson.test.ts
+++ src/domain/model/lesson/lesson.test.ts
@@ -1,17 +1,21 @@
 import { describe, expect, it } from "vitest";
+import { LessonCategoryId } from "../lesson-category/value-object/lesson-category-id";
 import { Lesson } from "./lesson";
 
 describe("Lesson", () => {
   it("Lessonの生成", () => {
-    const lesson = Lesson.create("DDD基礎", "課題：値オブジェクトとは何か？");
+    const lessonCategoryId = LessonCategoryId.create();
+    const lesson = Lesson.create(lessonCategoryId, "DDD基礎", "課題:値オブジェクトとは何か?");
 
     expect(lesson.id.value).toBeTypeOf("string");
+    expect(lesson.lessonCategoryId.equals(lessonCategoryId)).toBe(true);
     expect(lesson.name).toBe("DDD基礎");
-    expect(lesson.content).toBe("課題：値オブジェクトとは何か？");
+    expect(lesson.content).toBe("課題:値オブジェクトとは何か?");
   });
 
   it("Leesonのタイトル変更", () => {
-    const lesson = Lesson.create("Old name", "Content");
+    const lessonCategoryId = LessonCategoryId.create();
+    const lesson = Lesson.create(lessonCategoryId, "Old name", "Content");
 
     lesson.changeName("New name");
 
@@ -19,7 +23,8 @@ describe("Lesson", () => {
   });
 
   it("Lessonの課題内容変更", () => {
-    const lesson = Lesson.create("Lesson", "Old content");
+    const lessonCategoryId = LessonCategoryId.create();
+    const lesson = Lesson.create(lessonCategoryId, "Lesson", "Old content");
 
     lesson.changeContent("Updated content");
 
